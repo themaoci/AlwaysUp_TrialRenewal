d219caeac7e513436b35199205fe90c94a8fec2d
TheMaoci <themaoci@gmail.com>
2021-02-04 15:45:18 +0100

    Lol that was easy...

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Win32;
+using System;
+
+namespace AlwaysUp_TrialRenewal
+{
+    class Program
+    {
+        //1612621085
+        static void Main(string[] args)
+        {
+            long newTimestamp = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds() + 60 * 60 * 24 * 2;
+            Registry.SetValue(
+                @"HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Core Technologies Consulting\AlwaysUp", 
+                "Run",
+                newTimestamp
+                );
+        }
+    }
+}
